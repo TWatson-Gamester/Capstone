756f93824c27642d3989d8631a9c251d875130a2
TWatson-Gamester <twatson@student.neumont.edu>
2022-11-29 17:28:59 -0700

    Demon Added

--- Assets/Scripts/LevelObjects/LevelSpawn.cs
+++ Assets/Scripts/LevelObjects/LevelSpawn.cs
@@ -6,9 +6,12 @@ public class LevelSpawn : MonoBehaviour
 {
     [Header("Player 1 Characters")]
     public GameObject P1YBot;
+    public GameObject P1Demon;
     [Header("AI Characters")]
     public GameObject AIYBot1;
     public GameObject AIYBot2;
+    public GameObject AIDemon1;
+    public GameObject AIDemon2;
 
     public static int Player1Choice;
     public static int Player2Choice;
@@ -26,6 +29,12 @@ public class LevelSpawn : MonoBehaviour
             case 2:
                 Instantiate(AIYBot1, Player1Spawn.position, Player2Spawn.rotation);
                 break;
+            case 3:
+                Instantiate(P1Demon, Player1Spawn.position, Player1Spawn.rotation);
+                break;
+            case 4:
+                Instantiate(AIDemon1, Player1Spawn.position, Player2Spawn.rotation);
+                break;
         }
         switch (Player2Choice)
         {
@@ -34,6 +43,11 @@ public class LevelSpawn : MonoBehaviour
                 break;
             case 2:
                 Instantiate(AIYBot2, Player2Spawn.position, Player2Spawn.rotation);
+                break;
+            case 3:
+                break;
+            case 4:
+                Instantiate(AIDemon2, Player2Spawn.position, Player2Spawn.rotation);
                 break;
         }
     }
